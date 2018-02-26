6a003a39c25700f6660d84e4c949449cce4c4778
Moshe <moshehoa@amdocs.com>
2018-02-26 13:39:57 +0200

    Initial VNFTEST fw Issue-ID: VNFSDK-195
    Change-Id: I5abf0dd033e76e5225bb8271c0afaea325d741d9
    Signed-off-by: Moshe <moshehoa@amdocs.com>
    
    docker build
    
    Issue-ID: VNFSDK-195
    
    Change-Id: I25eb933504c0201e6c26477b540626fd515d2887
    
    Signed-off-by: Moshe <moshehoa@amdocs.com>
    
    fix requirements
    
    Issue-ID: VNFSDK-195
    Change-Id: I5907fa102bfbf9cb81d42e491c133b4fdbb0d6fd
    Signed-off-by: Moshe <moshehoa@amdocs.com>
    
    rm netifaces
    
    Issue-ID: VNFSDK-195
    Change-Id: I349d0c738442edfef256c90b06cbaeb446c1db13
    Signed-off-by: Moshe <moshehoa@amdocs.com>
    
    fix tox config
    
    IssueID: VNFTEST-195
    
    Change-Id: I5c0b0e0ab96cad1bdc56ab63860d794bfd15b5eb
    Signed-off-by: Moshe <moshehoa@amdocs.com>

---
+++ vnftest/onap/contexts/csar.py
@@ -0,0 +1,44 @@
+##############################################################################
+# Copyright 2018 EuropeanSoftwareMarketingLtd.
+# ===================================================================
+#  Licensed under the ApacheLicense, Version2.0 (the"License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under
+# the License
+##############################################################################
+
+import logging
+from vnftest.onap.contexts.base import Context
+
+LOG = logging.getLogger(__name__)
+
+
+class CSARContext(Context):
+    """Class that handle sdc info"""
+
+    __context_type__ = "CSAR"
+
+    def __init__(self):
+        self.csar_name = None
+        self.csar_id = None
+        self.csar_package_location = None
+        super(CSARContext, self).__init__()
+
+    def init(self, attrs):
+        """initializes itself from the supplied arguments"""
+        self.csar_name = attrs.get("csar_name")
+        self.csar_id = attrs.get("csar_id")
+        self.csar_package_location = attrs.get("csar_package_location")
+
+    def deploy(self):
+        """no need to deploy"""
+        pass
+
+    def undeploy(self):
+        """no need to undeploy"""
+        super(CSARContext, self).undeploy()
