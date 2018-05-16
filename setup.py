6dbe9eb859a24ee55331c4f15a4f49265563db4f
Chris Donley <christopher.donley@huawei.com>
2018-05-16 16:36:27 -0700

    Increment vnfsdk dovetail version
    Issue-ID: VNFSDK-267
    
    Change-Id: I9c3a32f5c3e181ca37cbd0618a8af16d17eceb5e
    Signed-off-by: Chris Donley <christopher.donley@huawei.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vnftest",
-    version="0.1.dev0",
+    version="1.1.0",
     packages=find_packages(),
     include_package_data=True,
     package_data={
